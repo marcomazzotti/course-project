e821e79cce437a8a82902324af7e1bf52042357c
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-08-10 13:31:43 +0200

    Add: recipe Service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,13 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe-list/recipe.model";
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is the description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
