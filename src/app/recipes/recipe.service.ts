5b80c3bf564838481f5945c7d2be940eed64fe53
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-08-11 11:32:21 +0200

    Add: ingredients in recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe-list/recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is the description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    new Recipe('A Test Recipe', 'This is the description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505', [
+      new Ingredient("Meat", 1),
+      new Ingredient("French Fries", 20)
+    ]),
   ];
 
   getRecipes() {
