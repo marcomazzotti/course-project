99815a7663a77df2e551521d6c14371b55233f76
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-08-16 16:24:44 +0200

    Add: routing for recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe-list/recipe.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import { __param } from 'tslib';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,15 +12,19 @@ import { Recipe } from '../recipe-list/recipe.model';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id)
+    })
   }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-
 }
