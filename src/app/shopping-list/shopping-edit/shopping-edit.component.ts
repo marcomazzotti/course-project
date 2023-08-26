49303c079e2df08fed72bd4edceb223d32b0ac3a
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-08-26 16:08:28 +0200

    edit: shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,14 +1,15 @@
 import {
   Component,
   OnInit,
-  ElementRef,
-  ViewChild,
+  OnDestroy,
+  ViewChild
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgFor } from '@angular/common';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -16,16 +17,52 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('f') slForm: NgForm
+  subscription: Subscription
+  editMode = false
+  editedItemIndex: number
+  editedItem: Ingredient
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true
+        this.editedItemIndex = index
+        this.editedItem = this.slService.getIngredient(index)
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
+      }
+    )
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient)
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient)
+    } else {
+      this.slService.addIngredient(newIngredient)
+    }
+    this.editMode = false
+    form.reset()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe
+  }
+
+  onClear() {
+    this.slForm.reset()
+    this.editMode = false
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
   }
 }
 
